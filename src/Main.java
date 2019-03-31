e56ecbb2d4a67536490888f26b1e2cb948684efc
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-03-31 12:03:13 +0200

    added total head movement calculation

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,20 @@ public class Main {
     private static final int
         seriesLength = 10,
         maxDiskLocation = 100,
-        maxArrivalTime = 0,
+        maxArrivalTime = 10,
         priorityRequests = 10;
 
 
     private static void printRequests(ArrayList<Request> requests) {
+        System.out.println("Generated requests:");
         requests.sort((req1, req2) -> req1.getArrivalTime() - req2.getArrivalTime());
         for (Request request : requests) {
-            System.out.println("ID: " + request.getId() + "; Time: " + request.getArrivalTime() + "; Address: " + request.getDiskLocation() + "; Priority: " + request.getPriority());
+            System.out.println(
+                "ID: " + request.getId() +
+                "; Time: " + request.getArrivalTime() +
+                "; Address: " + request.getDiskLocation() +
+                "; Priority: " + request.getPriority()
+            );
             System.out.println("-----------");
         }
         System.out.println();
@@ -35,7 +41,7 @@ public class Main {
         Scheduler scheduler = new Scheduler();
 
         int initialHeadPosition = RandomNums.randomInt(0, maxDiskLocation);
-        System.out.println("Initial head position: " + initialHeadPosition);
+        System.out.println("Initial head position: " + initialHeadPosition + "\n");
 
         ArrayList<Integer> fcfsResult = scheduler.run(generatedSeries, initialHeadPosition, new FCFS());
         ArrayList<Integer> sstfResult = scheduler.run(generatedSeries, initialHeadPosition, new SSTF());
@@ -44,22 +50,22 @@ public class Main {
         ArrayList<Integer> edfResult = scheduler.run(generatedSeries, initialHeadPosition, new EDF());
         ArrayList<Integer> fdscanResult = scheduler.run(generatedSeries, initialHeadPosition, new FDSCAN());
 
-        System.out.println("FCFS result:");
+        System.out.println("\n-- FCFS result --");
         ResultsAnalyzer.analyzeAndPrint(fcfsResult);
 
-        System.out.println("SSTF result:");
+        System.out.println("\n-- SSTF result --");
         ResultsAnalyzer.analyzeAndPrint(sstfResult);
 
-        System.out.println("SCAN result:");
+        System.out.println("\n-- SCAN result --");
         ResultsAnalyzer.analyzeAndPrint(scanResult);
 
-        System.out.println("C-SCAN result:");
+        System.out.println("\n-- C-SCAN result --");
         ResultsAnalyzer.analyzeAndPrint(cscanResult);
 
-        System.out.println("EDF result:");
+        System.out.println("\n-- EDF result --");
         ResultsAnalyzer.analyzeAndPrint(edfResult);
 
-        System.out.println("FD-SCAN result:");
+        System.out.println("\n-- FD-SCAN result --");
         ResultsAnalyzer.analyzeAndPrint(fdscanResult);
     }
 }
--- src/util/ResultsAnalyzer.java
+++ src/util/ResultsAnalyzer.java
@@ -3,11 +3,21 @@ package util;
 import java.util.ArrayList;
 
 public class ResultsAnalyzer {
+    private static int calcTotalHeadMovement(ArrayList<Integer> headPath) {
+        int result = 0;
+        for (int i = 0; i < headPath.size() - 1; i++) {
+            result += Math.abs(headPath.get(i) - headPath.get(i+1));
+        }
+
+        return result;
+    }
+
     public static void analyzeAndPrint(ArrayList<Integer> headPath) {
+       System.out.println("Head path:");
        for (int position : headPath) {
            System.out.print(position + " | ");
        }
 
-       System.out.println();
+       System.out.println("\nTotal head movement: " + calcTotalHeadMovement(headPath));
     }
 }
